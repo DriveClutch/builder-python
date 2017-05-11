db63469b701b2e4363c688ddd17632fc5134fe51
Greg Rickman <rickman.greg@gmail.com>
2017-05-11 10:23:00 -0400

    cleaning out clutch_python_libs during run [#144531971]

--- python.sh
+++ python.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 clutch_lib_names=(`find . -name "*requirements.txt" -print0 | xargs -0 grep -h clutch`)
 
+rm -rf clutch_python_libs
+mkdir clutch_python_libs
+
 for lib in "${clutch_lib_names[@]}"
 do
         pip download --index-url=https://$BINTRAY_USER:$BINTRAY_PASSWORD@driveclutch.bintray.com/clutch-python -d clutch_python_libs $lib
